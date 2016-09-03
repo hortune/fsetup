83e7feeedefad8828eab2b8e73b00580a55500a4
hortune <philip20336@gmail.com>
2016-09-03 00:08:07 -0700

    deal with the nvm problem

--- node_install.sh
+++ node_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | sudo bash
+wget -qO- https://raw.githubusercontent.com/xtuple/nvm/master/install.sh | sudo bash
 
-echo "export NVM_DIR=\"\$HOME/.nvm\"" >> ~/.bashrc
-echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"" >> ~/.bashrc  # 讀取 NVM
+#echo "export NVM_DIR=\"\$HOME/.nvm\"" >> ~/.bashrc
+#echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"" >> ~/.bashrc  # 讀取 NVM
